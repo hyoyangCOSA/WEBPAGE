a1323a1af13b88043884ccd32ee42f330200a72c
COSA <cosacosciai@gmail.com>
2024-05-06 16:52:26 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
@@ -13,6 +14,12 @@ let today = new Date();
 let Year = today.getFullYear();
 let Month = today.getMonth() + 1;
 let Day = today.getDate();
+if (Month < 10) {
+    Month = '0' + Month;
+}
+if (Day < 10) {
+    Day = '0' + Day;
+}
 let Hour = today.getHours();
 let Minute = today.getMinutes();
 let Second = today.getSeconds();
@@ -22,24 +29,14 @@ let todayurl = `https://hyoyang.goeic.kr/meal/view.do?menuId=9562&year=${Year}&m
 console.log(todayurl);
 
 
+function templateHTML(title, css, img, topic_item, topic1) {
 
-const getHtmltodaymenu = async () => {
-    try{
-        return await axios.get(todayurl);
-    } catch (err) {
-        console.log(error);
-    }
-}
-
-
-function templateHTML(title, css, img, topic_item, topic1){
-    
-    for(let i = 0;i<topic_item.length;i++){
+    for (let i = 0; i < topic_item.length; i++) {
         topic_item[i] = `<p class="topic_item">${topic_item[i]}</p>`;
     }
-    
+
     let template =
-    `
+        `
     <!DOCTYPE html>
     <html lang="ko">
     <head>
@@ -61,6 +58,9 @@ function templateHTML(title, css, img, topic_item, topic1){
                 <li class="header_item"><a href="/?id=STUDY">STUDY</a></li>
                 <li class="header_item"><a href="/?id=UPDATE">UPDATE</a></li>
             </ul>
+            <div class="header_descript">
+            <span>임시로 넣은걸까 아닐까</span>
+            </div>
         </div>
         <div>${img}</div>
         <div id="topic">
@@ -93,7 +93,7 @@ function templateHTML(title, css, img, topic_item, topic1){
                         //markers: true, 
                     },
                     opacity: 1,                 
-                    y: -100,
+                    y: -50,
                     duration: 0.01,
                 });
             });
@@ -108,69 +108,68 @@ function templateHTML(title, css, img, topic_item, topic1){
     `;
     return template;
 }
-
-/*<div>
-                <a href='/?id=HOME' target='_blank' ><img src='COSALOGO.png' border='0' alt='COSA' id="header_img" /></a>
-                
-                </div>*/
-
-
-
-function readCSS(){
+const getHtmltodaymenu = async () => {
+    try {
+        return await axios.get(todayurl);
+    } catch (err) {
+        console.log(error);
+    }
+}
+function readCSS() {
     let csslist = fs.readdirSync('./css');
     let css = ``;
-    for(let i = 0;i<csslist.length;i++){
+    for (let i = 0; i < csslist.length; i++) {
         css += fs.readFileSync(`css/${csslist[i]}`, 'utf8');
     }
     return css;
 }
 
-function settopic(dir){
+function settopic(dir) {
     let topic_itemList = fs.readdirSync(`./${dir}`);
-    for(let i = 0 ; i < topic_itemList.length ; i++){
+    for (let i = 0; i < topic_itemList.length; i++) {
         var item = fs.readFileSync(`${dir}/${topic_itemList[i]}`, 'utf8');
         topic_item.push(item);
     }
 }
 
-var app = http.createServer(function(req, res){
-    
+var app = http.createServer(function (req, res) {
+
     let _url = req.url
     let queryData = url.parse(_url, true).query;
     let title = queryData.id;
     let pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-        fs.readFile('index.html', 'utf8', (err, data)=>{
-            
+    if (pathname === '/') {
+        fs.readFile('index.html', 'utf8', (err, data) => {
+
             //메뉴 가져오기
-            
+
             getHtmltodaymenu()
-             .then(html => {
-                let menullist = [];
-                let $ = cheerio.load(html.data);
-                let $lunchmenu = $("#form > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span");
-                let $dinnermenu = $("#form > div > table > tbody > tr:nth-child(2) > td:nth-child(3) > span");
-                
-                lunchmenu = $lunchmenu.text();
-                dinnermenu = $dinnermenu.text();
-                
-                lunchmenu = lunchmenu.replace(/\s/g, "").replace(/\d/g, '').replace(/\./g, "").replace(/\"/g, "").replace(/\(|\)/g, '').split("ㆍ");
-                lunchmenuS = new Set(lunchmenu);
-                lunchmenu = [...lunchmenuS];
-                lunchmenu = lunchmenu.join("<br>");
-                fs.writeFileSync('todayLunchMenu.txt', lunchmenu);
-                dinnermenu = dinnermenu.replace(/\s/g, "").replace(/\d/g, '').replace(/\./g, "").replace(/\"/g, "").replace(/\(|\)/g, '').split("ㆍ");
-                dinnermenuS = new Set(dinnermenu);
-                dinnermenu = [...dinnermenuS];
-                dinnermenu = dinnermenu.join("<br>");
-                //console.log(dinnermenu);
-                fs.writeFileSync('todayDinnerMenu.txt', dinnermenu);
-            })       
-            
+                .then(html => {
+                    let menullist = [];
+                    let $ = cheerio.load(html.data);
+                    let $lunchmenu = $("#form > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span");
+                    let $dinnermenu = $("#form > div > table > tbody > tr:nth-child(2) > td:nth-child(3) > span");
+
+                    lunchmenu = $lunchmenu.text();
+                    dinnermenu = $dinnermenu.text();
+
+                    lunchmenu = lunchmenu.replace(/\s/g, "").replace(/\d/g, '').replace(/\./g, "").replace(/\"/g, "").replace(/\(|\)/g, '').split("ㆍ");
+                    lunchmenuS = new Set(lunchmenu);
+                    lunchmenu = [...lunchmenuS];
+                    lunchmenu = lunchmenu.join("<br>");
+                    fs.writeFileSync('todayLunchMenu.txt', lunchmenu);
+                    dinnermenu = dinnermenu.replace(/\s/g, "").replace(/\d/g, '').replace(/\./g, "").replace(/\"/g, "").replace(/\(|\)/g, '').split("ㆍ");
+                    dinnermenuS = new Set(dinnermenu);
+                    dinnermenu = [...dinnermenuS];
+                    dinnermenu = dinnermenu.join("<br>");
+                    //console.log(dinnermenu);
+                    fs.writeFileSync('todayDinnerMenu.txt', dinnermenu);
+                })
+
             let img = `<img src="https://ifh.cc/g/NG2PS7.png" alt="banner">`;
             topic_item = [];
             let title = "COSA-";
-            if(queryData.id === "HOME"){
+            if (queryData.id === "HOME") {
                 title += `${queryData.id}`;
                 settopic('topic_items');
                 let todayLunchMenu = fs.readFileSync('todayLunchMenu.txt', 'utf8');
@@ -178,38 +177,40 @@ var app = http.createServer(function(req, res){
                 topic_item.push(`<ul class="foodmenu"><li id="menu_introtext">오늘의 급식 →</li> <li>중식 : ${todayLunchMenu}</li><li>석식 : ${todayDinnerMenu}</li></span>`);
                 res.writeHead(200);
                 res.end(templateHTML(title, readCSS(), img, topic_item, `<p class="topic_time">Time of the Site : ${Year}-${Month}-${Day}</p>`));
-            }       
-            else if(queryData.id === 'INFO'){
+            }
+            else if (queryData.id === 'INFO') {
                 title += `${queryData.id}`;
                 //img = null;
-                topic_item.push(`11년간의 전통을 자랑하는 컴퓨터 공학 동아리 COSA<br>
-                COSA의 CO는 Computer Science에서<br>
-                A는 Artifical intelligence에서 따왔습니다`);
+                topic_item.push(`
+                11년간의 전통을 자랑하는 컴퓨터 공학 동아리 COSA<br>
+                COSA의 COS는 COmputer Science에서<br>
+                A는 Artifical intelligence에서 따왔습니다
+                `);
                 res.writeHead(200);
                 res.end(templateHTML(title, readCSS(), img, topic_item, ""));
             }
-            else if(queryData.id === 'EVENT'){
+            else if (queryData.id === 'EVENT') {
                 title += `${queryData.id}`;
                 topic_item.push("임시창");
                 res.writeHead(200);
                 res.end(templateHTML(title, readCSS(), img, topic_item, ""));
             }
-            else if(queryData.id === 'STUDY'){
+            else if (queryData.id === 'STUDY') {
                 title += `${queryData.id}`;
-                settopic('study_items'); 
+                settopic('study_items');
                 res.writeHead(200);
                 res.end(templateHTML(title, readCSS(), img, topic_item, ""));
             }
-            else if(queryData.id === 'UPDATE'){
+            else if (queryData.id === 'UPDATE') {
                 title += `${queryData.id}`;
-                settopic('update_items');    
+                settopic('update_items');
                 res.writeHead(200);
                 res.end(templateHTML(title, readCSS(), img, topic_item, ""));
             }
-            else if(queryData.id === undefined){
-                res.writeHead(302, {location: `/?id=HOME`});
+            else if (queryData.id === undefined) {
+                res.writeHead(302, { location: `/?id=HOME` });
                 res.end();
-            }else{
+            } else {
                 res.writeHead(404);
                 res.end("Not Found");
             }
@@ -219,6 +220,6 @@ var app = http.createServer(function(req, res){
         res.end("Not Found");
     }
 
-    
+
 });
 app.listen(3000);
